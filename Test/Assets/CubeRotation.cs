1d1675177db11b96218d13f7f3b45c6a856ab368
Kacper <kacperniwczyk@gmail.com>
2020-08-15 15:18:21 +0200

    initial

---
+++ Test/Assets/CubeRotation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    private float rotationValue;
+    public float rotationSpeed = 10;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 300;
+        rotationValue = rotationSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationValue, rotationValue, rotationValue);
+    }
+}
